4a0527bccfae875f5df84bd27aeaf5f2242c12c9
Asish Patra <patraasish66@gmail.com>
2024-12-15 22:57:56 +0530

    AlertEventReceiver Project setup done

---
+++ AlertEventReceiver/src/main/java/com/alertreceiver/AlertEventReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.alertreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertEventReceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertEventReceiverApplication.class, args);
+	}
+
+}
---
+++ AlertEventReceiver/src/test/java/com/alertreceiver/AlertEventReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alertreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertEventReceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
